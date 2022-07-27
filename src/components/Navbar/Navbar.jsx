0550512859d096c94d2bac83f6a81cd1f69cd00e
Leandro <lean.arbelo.dev@gmail.com>
2022-07-27 19:42:16 -0300

    nuevos proyectos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar({openMenu, setOpenMenu}) {
                 </a>
               <div className="item-container">
                 <CallIcon className="icon"/>
-                <span><a href='tel:+59892804870'>+598 92804870</a></span>
+                <span><a href='tel:+5493515912166'>+549 3515912166</a></span>
               </div>
               <div className="item-container">
                 <MailIcon className="icon"/>
--- src/data.js
+++ src/data.js
@@ -7,16 +7,22 @@ export const reactPortfolio = [
     },
     {
         id: '2',
+        title: 'Cinematics',
+        img: 'assets/cinematics.png',
+        url: 'https://cinematics-la.herokuapp.com/'
+    },
+    {
+        id: '3',
+        title: 'TODO App',
+        img: 'assets/TODO-app.png',
+        url: 'https://leanarbelo22.github.io/app-tareas/'
+    },
+    {
+        id: '4',
         title: 'Frog Backend MERN Stack',
         img: 'assets/frog-backend.png',
         url: 'https://github.com/LeanArbelo22/FROG-Backend'
     },
-    {
-        id: '3',
-        title: 'TODO React App, useContext, useReducer',
-        img: 'assets/TODO-app.png',
-        url: 'https://leanarbelo22.github.io/app-tareas/'
-    }
 
 ];
 
@@ -29,28 +35,10 @@ export const staticPortfolio = [
     },
     {
         id: '2',
-        title: 'Shopping Cart with VanillaJS',
-        img: 'assets/cart-js.png',
-        url: 'https://leanarbelo22.github.io/Carrito-VanillaJS/'
-    },
-    {
-        id: '3',
         title: 'VanillaJS, CSS from JavaScript30 Course',
         img: 'assets/vanilla30.png',
         url: 'https://leanarbelo22.github.io/VanillaJS30/'
-    },
-    {
-        id: '4',
-        title: 'CV 2022 made with CSS',
-        img: 'assets/cv2022.png',
-        url: 'https://github.com/LeanArbelo22/CV-2022/'
-    },
-    {
-        id: '5',
-        title: 'CV 2021 made with Bootstrap',
-        img: 'assets/cv2021.png',
-        url: 'https://github.com/LeanArbelo22/Curriculum/'
-    }   
+    }
 ];
 
 export const pythonPortfolio = [
@@ -65,15 +53,51 @@ export const pythonPortfolio = [
 export const jsPortfolio = [
     {
         id: '1',
+        title: 'Frog Static Web Front/Back',
+        img: 'assets/frog-html.png',
+        url: 'https://leanarbelo22.github.io/FROG-Proyecto/public/index.html'
+    },
+    {
+        id: '2',
         title: 'Shopping Cart with VanillaJS',
         img: 'assets/cart-js.png',
         url: 'https://leanarbelo22.github.io/Carrito-VanillaJS/'
     },
     {
-        id: '2',
-        title: 'VanillaJS, CSS from JavaScript30 Course',
-        img: 'assets/vanilla30.png',
-        url: 'https://leanarbelo22.github.io/VanillaJS30/'
+        id: '3',
+        title: 'Gestion de notas de alumnos',
+        img: 'assets/notas.png',
+        url: 'https://leanarbelo22.github.io/gestion-notas-alumnos/'
+    },
+    {
+        id: '4',
+        title: 'Drum Kit',
+        img: 'assets/drum.png',
+        url: 'https://leanarbelo22.github.io/VanillaJS30/1.%20Drum%20Kit/index.html'
+    },
+    {
+        id: '5',
+        title: 'Editar imagenes con JS y CSS',
+        img: 'assets/scope-css.png',
+        url: 'https://leanarbelo22.github.io/VanillaJS30/3.%20Scope%20CSS/index.html'
+    },
+    {
+        id: '6',
+        title: 'Galeria de paneles flex',
+        img: 'assets/panels.png',
+        url: 'https://leanarbelo22.github.io/VanillaJS30/5.%20Flex%20Panel%20Gallery/index.html'
+    },
+    {
+        id: '7',
+        title: 'Reproductor de video personalizado',
+        img: 'assets/reproductor.png',
+        url: 'https://leanarbelo22.github.io/VanillaJS30/11.%20Reproductor%20de%20video/index.html'
+    },
+    {
+        id: '8',
+        title: 'Slide in on Scroll',
+        img: 'assets/slide.png',
+        url: 'https://leanarbelo22.github.io/VanillaJS30/11.%20Reproductor%20de%20video/index.html'
     }
 ];
 
@@ -89,5 +113,11 @@ export const cvPortfolio = [
         title: 'CV 2021 made with Bootstrap',
         img: 'assets/cv2021.png',
         url: 'https://leanarbelo22.github.io/Curriculum/'
+    },
+    {
+        id: '3',
+        title: 'CV 2022 with Sass',
+        img: 'assets/cv-sass.png',
+        url: 'https://leanarbelo22.github.io/CV-Sass/'
     }
 ];
